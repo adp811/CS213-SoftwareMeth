389aef978d784843041b5bb9a6e1594415f4fd34
Aryan Patel <adp2582@gmail.com>
2023-02-07 14:31:39 -0500

    finished initial code for Major enum

--- Project1/src/rostermanager/Major.java
+++ Project1/src/rostermanager/Major.java
@@ -1,4 +1,26 @@
 package rostermanager;
 
 public enum Major {
+    CS    ("01:198","SAS"),
+    MATH  ("01:640","SAS"),
+    EE    ("14:332","SOE"),
+    ITI   ("04:547","SC&I"),
+    BAIT  ("33:136","RBS");
+
+    private final String code;
+    private final String school;
+
+    Major(String code, String school){
+        this.code = code;
+        this.school = school;
+    }
+
+    private String getCode(){
+        return code;
+    }
+
+    private String getSchool(){
+        return school;
+    }
+
 }
