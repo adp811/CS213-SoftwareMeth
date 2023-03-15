3c8d6fa85603cf4f83265bf5a3d88af005052a00
Aryan Patel <adp2582@gmail.com>
2023-03-15 19:03:50 -0400

    added fxid to elements on roster tab, configured neccesary visibility options

--- Project3/src/main/java/com/tuitionmanager/project3/TuitionManagerController.java
+++ Project3/src/main/java/com/tuitionmanager/project3/TuitionManagerController.java
@@ -1,14 +1,81 @@
 package com.tuitionmanager.project3;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 
 public class TuitionManagerController {
+
+    /**
+     * JavaFX Elements from Roster Tab
+     */
     @FXML
-    private Label welcomeText;
+    private TextField firstNameTextField, lastNameTextField, creditsCompletedTextField,
+                      fileNameTextField;
+    @FXML
+    private Button addButton, removeButton, changeMajorButton, loadFromFileButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private RadioButton baitRadioButton, csRadioButton, eceRadioButton, itiRadioButton,
+                        mathRadioButton;
+    @FXML
+    private RadioButton residentRadioButton, nonResidentRadioButton;
+
+    @FXML
+    private RadioButton triStateRadioButton, internationalRadioButton;
+
+    @FXML
+    private RadioButton nyStateRadioButton, ctStateRadioButton;
+
+    @FXML
+    private ToggleGroup statusGroup, nonResStatusGroup;
+
+    @FXML
+    private HBox triStateSelectionView, internationalSelectionView;
+
+    @FXML
+    private CheckBox studyAbroadCheckBox;
+
+    @FXML
+    private DatePicker dateOfBirthPicker;
+
+    /**
+     * Persistent JavaFX Elements
+     */
+    @FXML
+    private TextArea consoleTextArea;
+
+    /**
+     * needs comments
+     */
+    @FXML
+    private void initialize() {
+        statusGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                if (((RadioButton) newValue).getText().equals("Non-Resident")) {
+                    triStateSelectionView.setVisible(true);
+                    internationalSelectionView.setVisible(true);
+                } else {
+                    triStateSelectionView.setVisible(false);
+                    internationalSelectionView.setVisible(false);
+                }
+            }
+        });
+
+        nonResStatusGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                if (((RadioButton) newValue).getText().equals("TriState")) {
+                    nyStateRadioButton.setVisible(true);
+                    ctStateRadioButton.setVisible(true);
+                    studyAbroadCheckBox.setVisible(false);
+
+                } else {
+                    nyStateRadioButton.setVisible(false);
+                    ctStateRadioButton.setVisible(false);
+                    studyAbroadCheckBox.setVisible(true);
+                }
+            }
+        });
+
     }
 }
