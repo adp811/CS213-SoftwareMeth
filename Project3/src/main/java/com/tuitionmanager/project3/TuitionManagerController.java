259f7c8f74a4cf7b65c9f33707dc4737e6e53c11
Aryan Patel <adp2582@gmail.com>
2023-03-16 18:29:32 -0400

    finished event handlers for remove, change major, started work on the load from file button

--- Project3/src/main/java/com/tuitionmanager/project3/TuitionManagerController.java
+++ Project3/src/main/java/com/tuitionmanager/project3/TuitionManagerController.java
@@ -2,8 +2,14 @@ package com.tuitionmanager.project3;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
 
 public class TuitionManagerController {
 
@@ -28,26 +34,12 @@ public class TuitionManagerController {
     private Roster roster;
     private Enrollment enrollment;
 
-    /**
-     * JavaFX Elements from Roster Tab
-     */
+
     @FXML
     private TextField firstNameTextField, lastNameTextField, creditsCompletedTextField,
                       fileNameTextField;
     @FXML
-    private Button addButton, removeButton, changeMajorButton, loadFromFileButton;
-
-    @FXML
-    private RadioButton baitRadioButton, csRadioButton, eceRadioButton, itiRadioButton,
-                        mathRadioButton;
-    @FXML
-    private RadioButton residentRadioButton, nonResidentRadioButton;
-
-    @FXML
-    private RadioButton triStateRadioButton, internationalRadioButton;
-
-    @FXML
-    private RadioButton nyStateRadioButton, ctStateRadioButton;
+    private RadioButton triStateRadioButton, nyStateRadioButton, ctStateRadioButton;
 
     @FXML
     private ToggleGroup statusGroup, nonResStatusGroup, stateGroup, majorGroup;
@@ -61,11 +53,10 @@ public class TuitionManagerController {
     @FXML
     private DatePicker dateOfBirthPicker;
 
-    /**
-     * Persistent JavaFX Elements
-     */
+
     @FXML
     private TextArea consoleTextArea;
+
 
     /**
      * needs comments, regex
@@ -98,7 +89,6 @@ public class TuitionManagerController {
             consoleTextArea.setText("Credits completed invalid: no value entered!");
             return false;
         }
-
         try {
             int credits = Integer.parseInt(creditsCompletedTextField.getText());
             if (credits < MIN_CREDITS) {
@@ -119,7 +109,7 @@ public class TuitionManagerController {
      */
     private boolean validateDateOfBirth() {
         if (dateOfBirthPicker.getValue() == null) {
-            consoleTextArea.setText("DOB invalid: no date selected!");
+            consoleTextArea.setText("DOB Invalid: no date selected!");
             return false;
         }
 
@@ -130,7 +120,7 @@ public class TuitionManagerController {
         Date dob = new Date(dateString), current = new Date();
 
         if (dob.equals(current) || dob.compareTo(current) > 0) {
-            consoleTextArea.setText("DOB invalid: cannot be current or future date!");
+            consoleTextArea.setText("DOB Invalid: cannot be current or future date!");
             return false;
         }
 
@@ -140,9 +130,8 @@ public class TuitionManagerController {
         } else if (current.getMonth() == dob.getMonth() && current.getDay() < dob.getDay()) {
             age--;
         }
-
         if (age < MIN_AGE) {
-            consoleTextArea.setText("DOB invalid: " + dob + " younger than 16 years old.");
+            consoleTextArea.setText("DOB Invalid: " + dob + " younger than 16 years old.");
             return false;
         }
 
@@ -170,7 +159,6 @@ public class TuitionManagerController {
                                         case "CT" -> { return TRISTATE_CT; }
                                     }
                                 }
-
                                 consoleTextArea.setText("Status Selection Invalid: please choose a state!");
                                 return UNDETERMINED_STUDENT;
                             }
@@ -184,7 +172,6 @@ public class TuitionManagerController {
                 }
             }
         }
-
         consoleTextArea.setText("Status Selection Invalid: please choose resident or non-resident!");
         return UNDETERMINED_STUDENT;
     }
@@ -200,7 +187,7 @@ public class TuitionManagerController {
      */
     private Major determineMajor(String inputMajor) {
         for(Major major : Major.values()) {
-            if(major.name().equals(inputMajor.toUpperCase())) {
+            if(major.name().equalsIgnoreCase(inputMajor)) {
                 return major;
             }
         }
@@ -217,6 +204,7 @@ public class TuitionManagerController {
 
         Profile profile = new Profile(arrInfo[1], arrInfo[0], new Date(arrInfo[2]));
         Major major = determineMajor(arrInfo[4]);
+
         int creditCompleted = Integer.parseInt(arrInfo[3]);
         int status = Integer.parseInt(arrInfo[5]);
 
@@ -231,12 +219,80 @@ public class TuitionManagerController {
         };
     }
 
+    /** rework
+     *
+     * @param roster Roster object containing roster array that we are searching through
+     * @param profile Profile object containing a student's information that we are trying
+     *                to look for
+     * @return Student object associated with the given Profile or null if it doesnt exist
+     */
+    private Student getStudent(Roster roster, Profile profile){
+        for(int i = 0; i < roster.getRosterSize(); i++) {
+            Student student = roster.getRoster()[i];
+            if (student.getProfile().equals(profile)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * This method takes care of printing the entire roster array associated with the
+     * inputted Roster object.
+     *
+     * @param roster Roster object which contains the roster array we want to print
+     * */
+    private void printRoster(Roster roster) {
+        for (int i = 0; i < roster.getRosterSize(); i++) {
+            System.out.println(roster.getRoster()[i].toString());
+        }
+    }
+
+    /**
+     *
+     * @param roster Roster object which contains the roster array we want to filter
+     *               through and print
+     * @param school String which contains the school name that we want use as a filter,
+     *               case-insensitive ("SAS", "SaS", "sas")
+     */
+    private void printRoster(Roster roster, String school) {
+        for(Major major : Major.values()) {
+            if(major.getSchool().equalsIgnoreCase(school)) {
+                System.out.println("* Students in " + school + " *");
+                for (int i = 0; i < roster.getRosterSize(); i++) {
+                    Student student = roster.getRoster()[i];
+                    if(student.getMajor().getSchool().equalsIgnoreCase(school)){
+                        System.out.println(student);
+                    }
+                }
+                System.out.println("* end of list **");
+                return;
+            }
+        }
+        System.out.println("School doesn't exist: " + school);
+    }
+
+    /** rework
+     *
+     * @param roster Roster object which contains the roster array that we are printing from
+     */
+    private void executePrintRoster(Roster roster) {
+        if (roster.getRosterSize() == MIN_ROSTER_SIZE) {
+            System.out.println("Student roster is empty!");
+        } else {
+            System.out.println("** Student roster sorted by last name, first name, DOB **");
+            roster.print();
+            printRoster(roster);
+            System.out.println("* end of roster *");
+        }
+    }
+
     /**
      * needs comments
-     * @param e
+     * @param event
      */
     @FXML
-    private void handleAddButtonClick (ActionEvent e) {
+    private void handleAddButtonClick (ActionEvent event) {
         if(!validateFullName() || !validateDateOfBirth() || !validateCreditsCompleted()) {
             return;
         }
@@ -248,39 +304,139 @@ public class TuitionManagerController {
 
         String studentInformation = firstNameTextField.getText()
                 + " " + lastNameTextField.getText()
-                + " " + dateOfBirthPicker.getValue().getMonth()
+                + " " + dateOfBirthPicker.getValue().getMonth().getValue()
                 + "/" + dateOfBirthPicker.getValue().getDayOfMonth()
                 + "/" + dateOfBirthPicker.getValue().getYear()
                 + " " + creditsCompletedTextField.getText()
                 + " " + ((RadioButton) majorGroup.getSelectedToggle()).getText()
                 + " " + status;
 
-        consoleTextArea.setText(studentInformation);
-
+        Student student;
+        if ((student = createStudent(studentInformation)) == null) {
+            return;
+        }
+        try {
+            if(!roster.add(student)) {
+                consoleTextArea.setText(student.getProfile().toString() + " is already in the roster.");
+            } else {
+                consoleTextArea.setText(student.getProfile().toString() + " added to the roster.");
+            }
+        } catch (Exception e) {
+            consoleTextArea.setText("Fatal Error: Student could not be added, please try again.");
+        }
     }
 
     /**
      * needs comments
-     * @param e
+     * @param event
      */
-    private void handleRemoveButtonClick (ActionEvent e) {
+    @FXML
+    private void handleRemoveButtonClick (ActionEvent event) {
+        if(!validateFullName() || !validateDateOfBirth()) {
+            return;
+        }
 
+        Profile searchProfile = new Profile(
+                lastNameTextField.getText().trim(),
+                firstNameTextField.getText().trim(),
+                new Date(
+                        dateOfBirthPicker.getValue().getMonth().getValue()
+                        + "/" + dateOfBirthPicker.getValue().getDayOfMonth()
+                        + "/" + dateOfBirthPicker.getValue().getYear()
+                )
+        );
+
+        Student student;
+        if((student = getStudent(roster, searchProfile)) == null) {
+            consoleTextArea.setText(searchProfile + " is not in the roster.");
+            return;
+        }
+        try {
+            if (roster.remove(student)) {
+                consoleTextArea.setText(student.getProfile().toString() + " removed from the roster.");
+            }
+        } catch (Exception e) {
+            consoleTextArea.setText("Fatal Error: Student could not be removed, please try again.");
+        }
     }
 
     /**
      * needs comments
-     * @param e
+     * @param event
      */
-    private void handleChangeMajorButtonClick (ActionEvent e) {
+    @FXML
+    private void handleChangeMajorButtonClick (ActionEvent event) {
+        if(!validateFullName() || !validateDateOfBirth()) {
+            return;
+        }
 
+        Profile searchProfile = new Profile(
+                lastNameTextField.getText().trim(),
+                firstNameTextField.getText().trim(),
+                new Date(
+                        dateOfBirthPicker.getValue().getMonth().getValue()
+                                + "/" + dateOfBirthPicker.getValue().getDayOfMonth()
+                                + "/" + dateOfBirthPicker.getValue().getYear()
+                )
+        );
+
+        Student student;
+        if((student = getStudent(roster, searchProfile)) == null) {
+            consoleTextArea.setText(searchProfile + " is not in the roster.");
+            return;
+        }
+        try {
+            RadioButton selectedMajorRadioButton = (RadioButton) majorGroup.getSelectedToggle();
+            if (selectedMajorRadioButton != null) {
+                String selectedOption = selectedMajorRadioButton.getText();
+                Major major = determineMajor(selectedOption);
+                if (major != null) {
+                    student.setMajor(major);
+                    consoleTextArea.setText(searchProfile + " major changed to " + major.name());
+                }
+            } else {
+                consoleTextArea.setText("Major Selection Invalid: no major is selected!");
+            }
+        } catch (Exception e) {
+            consoleTextArea.setText("Fatal Error: Student major could not be changed, please try again.");
+        }
+
+        executePrintRoster(roster);
     }
 
     /**
      * needs comments
-     * @param e
+     * @param event
      */
-    private void handleLoadFromFileButtonClick (ActionEvent e) {
+    @FXML
+    private void handleLoadFromFileButtonClick (ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FileChooser fc = new FileChooser();
 
+        fc.setTitle("Select roster text file");
+        fc.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "Text files (*.txt)", "*.txt");
+
+        fc.getExtensionFilters().add(extFilter);
+
+        File file = fc.showOpenDialog(stage);
+        if (file != null) {
+            try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+                // finish loading
+            }
+            System.out.println("Students loaded to the roster.");
+            fileNameTextField.setText(file.getName());
+            sc.close();
+
+            } catch (FileNotFoundException e) {
+                System.out.println("File Not Found: " + file.getName());
+            }
+        }
     }
 
     /**
@@ -288,7 +444,6 @@ public class TuitionManagerController {
      */
     @FXML
     private void initialize() {
-
         consoleTextArea.setText("Tuition Manager running...");
 
         roster = new Roster();
@@ -317,6 +472,16 @@ public class TuitionManagerController {
                     nyStateRadioButton.setVisible(false);
                     ctStateRadioButton.setVisible(false);
                     studyAbroadCheckBox.setVisible(true);
+                }
+            }
+        });
+
+        stateGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                if (((RadioButton) newValue).getText().equals("NY")) {
+                    triStateRadioButton.setSelected(true);
+                } else if (((RadioButton) newValue).getText().equals("CT")) {
+                    triStateRadioButton.setSelected(true);
                 }
             }
         });
